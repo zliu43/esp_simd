f030b91ee9aeef3304682b3066499c285c68ad8f
Michael Liu <mliu@mac.lan>
2025-09-18 22:43:02 -0700

    good enough, push it

--- components/esp_simd/examples/01_acessing_elements.c
+++ components/esp_simd/examples/01_acessing_elements.c
@@ -3,11 +3,11 @@
 void app_main(){
     /**
      * the vector struct stores the data buffer as a void*. It must be cast to the appropriate type before indexing.
-     * 
      */
     VECTOR_STACK_INIT(vec1, 8, DTYPE_INT32);
-    int32_t* vec1_data = (int32_t*)vec1.data;
-    vec1_data[0] = 2;
+    int32_t* vec1_data = (int32_t*)vec1.data;   // This cast has zero overhead. 
+
+    vec1_data[0] = 2;                           // vec1_data can be used as a normal int32_t array
     vec1_data[1] = 4;
 
     vec1_data[2] = vec1_data[0] * vec1_data[1]; 
